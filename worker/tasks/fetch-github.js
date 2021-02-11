f2bffc1e6109c5c03f2a0e8cfffe2e3b6d1ace44
Carter Conboy <conboy2001@gmail.com>
2021-02-10 22:17:53 -0500

    Added Redis Database to store Jobs, added basic filter algorithm to filter out senior roles

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,5 +1,11 @@
 var fetch = require('node-fetch');
 
+var redis = require("redis"), client = redis.createClient();
+
+const { promisify } = require("util");
+//const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
 const baseURL = 'https://jobs.github.com/positions.json';
 
 async function fetchGithub(){
@@ -8,6 +14,7 @@ async function fetchGithub(){
     const allJobs = [];
 
 
+    //fetchs all pages
     while(resultCount > 0){
         const res = await fetch(`${baseURL}?page=${onPage}`); 
         const jobs = await res.json();
@@ -19,12 +26,37 @@ async function fetchGithub(){
 
     }
 
+
     console.log('got ' + allJobs.length + " jobs");
+
+    //filter all 
+
+    const jrJobs = allJobs.filter(job =>{
+        const jobTitle = job.title.toLowerCase();
+        //Algorithm Logic
+
+        if (jobTitle.includes('senior') || 
+        jobTitle.includes('manager') || 
+        jobTitle.includes('sr.') ||
+        jobTitle.includes('architect')
+        ){
+            return false;
+        }
+
+        return true;
+        
+    });
+
+    console.log('Filtered down to', jrJobs.length);
+
+    //sets in redis
     
+    const success = await setAsync('github', JSON.stringify(jrJobs));
+
+    console.log({success});
 
 }
 
 
-fetchGithub();
 
 module.exports = fetchGithub; 
